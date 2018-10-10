4da9c7990570cc9619ea4b4a46c8e08763acf9fe
sansan83it00 <34060767+sansan83it00@users.noreply.github.com>
2018-10-10 17:03:30 +0200

    Début Class Animal

--- Animal.java
+++ Animal.java
@@ -1,4 +1,38 @@
 package zoo;
 
-public class Animal {
+abstract class Animal {
+
+	
+	protected String nom;
+	protected String sexe;
+	protected int poids;
+	protected int taille;
+	protected int age;
+	protected boolean faim;
+	protected String sommeil;
+	protected String sante;
+	
+	protected void manger(){
+		
+	}
+	
+	protected void son(){
+		
+	}
+	protected void soin(){
+		
+	}
+	
+	protected void reveil(){
+		
+	}
+		
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
